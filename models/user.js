e2eb9d260903aff9dd0680d04cb5fb72347eb75a
Gabriel <santosdalbelo@gmail.com>
2019-07-25 15:25:08 -0300

    commit models

---
+++ models/user.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    name: String,
+    description: String
+});
+
+userSchema.pre('save', function(next) {
+        var user = this
+
+        if(!user.isModified('password'))
+            return next()
+
+        bcrypt.hash(user.password, null, null, (err, hash) => {
+            if(err) return next(err)
+
+            user.password = hash;
+            next();
+        });
+})
+
+module.exports = mongoose.model('User', userSchema);
